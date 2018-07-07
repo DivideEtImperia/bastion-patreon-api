ae1c03276b52809239861c17935fc70dffcf6d34
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-07-07 05:39:43 +0000

    typings: added

---
+++ typings/index.d.ts
@@ -0,0 +1,20 @@
+﻿import * as request from "request-promise-native";
+declare class PatreonAPI {
+    clientID: string;
+    clientSecret: string;
+    creatorAccessToken: string;
+    creatorRefreshToken: string;
+    requestOptions: request.RequestPromiseOptions;
+    constructor(credentials: PatreonAPICredentials);
+    refreshAccessToken(): Promise<object>;
+    getPatrons(): Promise<object>;
+    getPatronsDiscord(): Promise<object>;
+}
+interface PatreonAPICredentials {
+    clientID: string;
+    clientSecret: string;
+    creatorAccessToken: string;
+    creatorRefreshToken: string;
+}
+export { PatreonAPI };
+//# sourceMappingURL=index.d.ts.map
